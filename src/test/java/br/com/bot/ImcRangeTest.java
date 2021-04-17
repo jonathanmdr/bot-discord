45c86f227adfd389e09dd3d5b1fe1fd63501192d
Jonathan Henrique Medeiros <jonathan.medeiros@objective.com.br>
2021-04-17 13:50:55 -0300

    ImcRangeTest class

---
+++ src/test/java/br/com/bot/ImcRangeTest.java
@@ -0,0 +1,45 @@
+package br.com.bot;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class ImcRangeTest {
+
+    @Test
+    public void getImcRange_whenUnderWeightImcRange_thenReturnFirstRecordOnTheListRanges() {
+        Optional<ImcRange> imcRange = ImcRange.getImcRange(10.0);
+
+        Assertions.assertThat(imcRange.get().getMessage()).isEqualTo("Se cuida, se bater um vento meio forte tu voa hahah");
+    }
+
+    @Test
+    public void getImcRange_whenNormalWeightImcRange_thenReturnSecondRecordOnTheListRanges() {
+        Optional<ImcRange> imcRange = ImcRange.getImcRange(20.0);
+
+        Assertions.assertThat(imcRange.get().getMessage()).isEqualTo("Tá tranquilo, pode comer doritos com coca!");
+    }
+
+    @Test
+    public void getImcRange_whenOverweightImcRange_thenReturnThirdRecordOnTheListRanges() {
+        Optional<ImcRange> imcRange = ImcRange.getImcRange(25.0);
+
+        Assertions.assertThat(imcRange.get().getMessage()).isEqualTo("Tu tá bem, anda malhando?");
+    }
+
+    @Test
+    public void getImcRange_whenGradeOneObesityImcRange_thenReturnFourthRecordOnTheListRanges() {
+        Optional<ImcRange> imcRange = ImcRange.getImcRange(30.0);
+
+        Assertions.assertThat(imcRange.get().getMessage()).isEqualTo("Já pensou em se exercitar um pouco?");
+    }
+
+    @Test
+    public void getImcRange_whenGradeTwoObesityImcRange_thenReturnFifthRecordOnTheListRanges() {
+        Optional<ImcRange> imcRange = ImcRange.getImcRange(40.0);
+
+        Assertions.assertThat(imcRange.get().getMessage()).isEqualTo("DIETA E ACADEMIA, JÁ!");
+    }
+
+}
